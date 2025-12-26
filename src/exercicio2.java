6faedebb9b693acf540e46efa257718298bb20b5
evellyn severo <evellyn.severo@zup.com.br>
2025-12-26 07:29:57 -0300

    exercicio2

---
+++ src/exercicio2.java
@@ -0,0 +1,20 @@
+public class exercicio2 {
+    public static void main(String[] args) { System.out.println("Hello and welcome!");
+        System.out.println("antes da troca :");
+        int a = 2;
+        int b = 5;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("depois da troca :");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+    }
+}
