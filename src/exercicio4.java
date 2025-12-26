5202cda90770a6c9625c4157008b9ab72f08bccc
evellyn severo <evellyn.severo@zup.com.br>
2025-12-26 07:49:19 -0300

    exercicio4

---
+++ src/exercicio4.java
@@ -0,0 +1,11 @@
+public class exercicio4 {
+    public static void main(String[] args) { System.out.println("Hello and welcome!");
+        char letraMinu = 'i';
+        char letraMaiu = Character.toUpperCase(letraMinu);
+        boolean evogal = true;
+
+        System.out.println("letra original: " + letraMinu);
+        System.out.println("Letra Maiuscula: " + letraMaiu);
+        System.out.println("é uma vogal: " + evogal);
+    }
+}
