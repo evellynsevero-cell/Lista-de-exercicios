0181fe4dc19d87e0ca0398c1dd3a07a251f3e247
evellyn severo <evellyn.severo@zup.com.br>
2025-12-26 08:15:09 -0300

    exercicio6

---
+++ src/exercicio6.java
@@ -0,0 +1,10 @@
+public class exercicio6 {
+    public static void main(String[] args) { System.out.println("Hello and welcome!");
+        int idade = 25;
+        if (idade >=18) {
+            System.out.println("Maior de idade");
+        }else{
+            System.out.println("Menor de idade");
+        }
+    }
+}
