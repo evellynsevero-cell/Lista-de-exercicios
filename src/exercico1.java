c49943c63174d363ac26bd30ce34ae5665fa7de1
evellyn severo <evellyn.severo@zup.com.br>
2025-12-26 07:09:22 -0300

    exercicio1

---
+++ src/exercico1.java
@@ -0,0 +1,12 @@
+
+public class exercico1 {
+    public static void main(String[] args) { System.out.println("Hello and welcome!");
+        double raio = 25.0;
+        double area = Math.PI *raio * raio;
+
+        System.out.println("valor do circulo do raio é " + raio);
+        System.out.println("Valor da area é " + area);
+
+
+        }
+    }
