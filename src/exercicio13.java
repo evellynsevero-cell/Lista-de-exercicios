5f8a77dccbcecea37776b5b1e18d5be035c2e24d
evellyn severo <evellyn.severo@zup.com.br>
2025-12-26 12:22:52 -0300

    exercicio13

---
+++ src/exercicio13.java
@@ -0,0 +1,27 @@
+public class exercicio13 {
+    public static void main(String[] args) { System.out.println("Hello and welcome!");
+        int valor = 350;
+        System.out.println("decomposição do valor " + valor + ":");
+        int notasde100 = valor / 100;
+        valor %= 100;
+        System.out.println(notasde100 + " nota(s) de 100");
+        int notasde50 = valor / 50;
+        valor %= 50;
+        System.out.println(notasde50 + " nota(s) de 50" );
+        int notasde20 = valor / 50;
+        valor %= 20;
+        System.out.println(notasde20 + " nota(s) de 20");
+        int notasde10 = valor / 10;
+        valor %= 10;
+        System.out.println(notasde10 + " nota(s) de 10");
+        int notasde5 = valor / 5;
+        valor %= 5;
+        System.out.println(notasde5 + " nota(s) de 5 ");
+        int notasde2 = valor / 2;
+        valor %= 2;
+        System.out.println(notasde2 + " nota(s) de 2 ");
+        int notasde1 = valor / 1;
+        valor %= 1;
+        System.out.println(notasde1 + " nota(s) de 1");
+    }
+}
