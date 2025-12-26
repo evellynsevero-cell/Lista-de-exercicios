a2b80393a0084caa5c2489290b9438c5972eccd8
evellyn severo <evellyn.severo@zup.com.br>
2025-12-26 08:08:12 -0300

    exercicio5

---
+++ src/exercicio5.java
@@ -0,0 +1,12 @@
+public class exercicio5 {
+    public static void main(String[] args) { System.out.println("Hello and welcome!");
+        String nomedoproduto = "Geladeira";
+        int quantidade = 2;
+
+        double preçounitario = 3500.00;
+        double total = quantidade * preçounitario;
+        System.out.printf("O valor total da %s é R$ %.2f%n",nomedoproduto,total );
+
+
+    }
+}
