24d3db43f93465aba2d7b1eabb625d3ce640a053
evellyn severo <evellyn.severo@zup.com.br>
2025-12-26 08:38:54 -0300

    exercicio7

---
+++ src/exercicio7.java
@@ -0,0 +1,16 @@
+public class exercicio7 {
+    public static void main(String[] args) { System.out.println("Hello and welcome!");
+        int numero = 10;
+        if (numero > 0 && numero % 2 ==0) {
+            System.out.println("Positivo e par");
+        }else if (numero > 0 && numero % 2 != 0) {
+            System.out.println("Positivo e impar");
+        }else if (numero == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Negativo");
+
+            }
+
+        }
+    }
